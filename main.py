0631a82220916e644382f2b537e234116751064f
Maykon <tecmaykonmarques@yahoo.com>
2023-02-21 18:24:31 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import socket
+import threading
+import rsa
+
+public_key, private_key = rsa.newkeys(1024)
+public_partner = None
+
+choice = input("Do you want to host (1) or to connect (2)? ")
+
+#choice = input("Você quer ser o host (1) ou se conectar (2)? ")
+
+if choice  == "1":
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('''aqui vai o ip da sua máquina / here goes your IP''', 9999))
+    server.listen()
+    client, _ = server.accept()
+    client.send(public_key.save_pkcs1("PEM"))
+    public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
+
+elif choice == "2":
+    client = server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.connect(('''aqui vai o ip da máquina que você está se conectando/ HEre goes the other PC ip''', 9999))
+    public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
+    client.send(public_key.save_pkcs1("PEM"))
+else:
+    exit()
+
+def sending_messages(c):
+    while True:
+        message = input("")
+        c.send(rsa.encrypt(message.encode(), public_partner))
+        print(("You: "+message))
+
+def receiving_messages(c):
+    while True:
+        print("Partner: " + rsa.decrypt(c.recv(2024),  private_key).decode())
+
+threading.Thread(target=sending_messages, args=(client,)).start()
+threading.Thread(target=receiving_messages, args=(client,)).start()
